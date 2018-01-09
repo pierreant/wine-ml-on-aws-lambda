3d2eb6129b5db9db480cedcd8d5c0e28f4bbc6c7
Pierre-Antoine Sondag <pasondag@gmail.com>
2018-01-09 02:32:26 +0100

    Initial commit adding code, libraries & read me.

---
+++ predict_wine.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+import pickle
+import random
+import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3_bucket = 'wine-prediction'
+
+def train_model(event, context):
+
+    model_name_prefix = 'model-'
+    dataset_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+
+    # Seeds to make the training repeatable
+    data_split_seed = 2
+    random_forest_seed = 2
+
+    logger.info(" > Importing Data < ")
+
+    data = pd.read_csv(
+        dataset_url, header='infer', na_values='?', sep=';')
+
+    # Uncomment to print data samples
+    # print(str(data.head()))
+
+    logger.info(" > Splitting in Training & Testing < ")
+
+    np.random.seed(data_split_seed)
+
+    data['split'] = np.random.randn(data.shape[0], 1)
+    split = np.random.rand(len(data)) <= 0.90
+
+    X_train = data[split].drop(['quality', 'split'], axis=1)
+    X_test = data[~split].drop(['quality', 'split'], axis=1)
+
+    y_train = data.quality[split]
+    y_test = data.quality[~split].as_matrix()
+
+    logger.info(" > Training Random Forest Model < ")
+
+    regressor = RandomForestRegressor(
+            max_depth=None, n_estimators=30, random_state=random_forest_seed)
+
+    regressor.fit(X_train, y_train)
+
+    logger.info(" > Saving model to S3 < ")
+
+    model_name = model_name_prefix + str(random.randint(0, 100000))
+    temp_file_path = '/tmp/' + model_name
+
+    with open(temp_file_path, 'wb') as f1:
+        pickle.dump(regressor, f1)
+
+    with open(temp_file_path, 'rb') as f2:
+        model_data = f2.read()
+
+    s3 = boto3.resource('s3')
+    s3_object = s3.Object(s3_bucket, model_name)
+    s3_object.put(Body=model_data)
+
+    logger.info("Model saved with name: " + model_name)
+
+    logger.info(" > Evaluating the Model < ")
+
+    y_predicted = regressor.predict(X_test)
+
+    logger.info(" Sample predictions on the test set < ")
+    for i in range(20):
+        logger.info("  label: " + str(y_test[i]) + " predicted: " + str(round(y_predicted[i], 2)))
+
+    logger.info(" Mean Absolute Error on full test set: " + str(round(metrics.mean_absolute_error(y_test, y_predicted), 3)))
+
+    return model_name
+
+
+
+def predict_with_model(event, context):
+
+    input_for_prediction = \
+        pd.DataFrame({
+            'fixed acidity': [event['fixed acidity']],
+            'volatile acidity': [event['volatile acidity']],
+            'citric acid': [event['citric acid']],
+            'residual sugar': [event['residual sugar']],
+            'chlorides': [event['chlorides']],
+            'free sulfur dioxide': [event['free sulfur dioxide']],
+            'total sulfur dioxide': [event['total sulfur dioxide']],
+            'density': [event['density']],
+            'pH': [event['pH']],
+            'sulphates': [event['sulphates']],
+            'alcohol': [event['alcohol']]})
+
+    logger.info("Input data for prediction:")
+    logger.info(str(input_for_prediction))
+
+    model_name = event['model name']
+
+    logger.info(" > Downloading model from S3 < ")
+
+    temp_file_path = '/tmp/' + model_name
+
+    s3 = boto3.client('s3')
+    s3.download_file(s3_bucket, model_name, temp_file_path)
+
+    logger.info(" > Loading model to memory < ")
+
+    with open(temp_file_path, 'rb') as f:
+        model = pickle.load(f)
+
+    logger.info(" > Predicting wine quality < ")
+
+    predicted_wine_grade = model.predict(input_for_prediction)
+
+    return str(round(predicted_wine_grade, 1))
+
---
+++ read_me.md
@@ -0,0 +1,99 @@
+
+# A Lambda to predict the quality of your wine
+
+## Scikit Learn Machine Learning Model on AWS Lambda
+
+This repo show how to train a Machine Learning model with Scikit Learn and Panda on AWS lambda. 
+The trained model will be able to predict the quality of a wine base on some parameters. 
+To train the model we are using the dataset available here https://archive.ics.uci.edu/ml/datasets/wine.
+
+
+## The general architecture
+
+The code is split into 2 Lambdas:
+
+ 1. The training Lambda
+    * Train the model
+    * Save the model to S3
+ 2. The prediction Lambda
+    * Read the model from S3
+    * Predict the value based on the Lambda input
+    
+both Lambdas are in the same file `predict_wine.py
+    
+ ## How do I run this in AWS Lambda`?
+ 
+ 1. Compress all the files in a single zip file.
+    * The folder structure need to remain as in the repo. 
+    The other folder are containing libraries (Numpy, Scikit Learn, Pandas) needed to run the code. 
+    They have been compiled for the Lambda runtime. They will not work locally.
+ 
+ 2. Upload the compressed file to one of your S3 bucket.
+ 
+ 3. Create an S3 bucket called 'wine-prediction' to save the models.
+ 
+ 4. In IAM 
+    1. Create a policy that enable to read and write object to your S3 bucket. The policy should look similar to:
+    ``
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Sid": "SomeSid01",
+        "Effect": "Allow",
+        "Action": [
+            "s3:PutObject",
+            "s3:GetObject"
+        ],
+        "Resource": "arn:aws:s3:::wine-prediction/*"
+    }
+]
+}
+``
+    
+    2. Create an IAM role (lets call it ``wine-s3-rw`` ), and attached the created policy.
+    
+  5. Create a lambda function 
+     * in "Function Code"
+        * use the uploaded zip as code
+        * use Python 2.7 as runtime
+        * see below for the handler  
+     * in "Execution role"
+        * select "choose an existing role"
+        * use the created  "wine-s3-rw" role
+     * in "Basic settings"
+        * make sure to give decent amount of memory (eg 800 MB)
+        * give a couple of second for the lambda to finish (eg 3)
+  
+  6. The lambda that will create the model is triggered by using `predict_wine.predict_with_model` as handler.
+  
+     * Input: none
+     * Returns: the saved model name (example: model-12408`)
+   
+  7. Once the model created. The prediction can be done by using `predict_wine.predict_with_model` as handler.
+    
+     * Input: json with the model name outputed in the previous step and the various information about the wine to predict the quality from. See example below:
+     
+     ``
+     {
+        "model name": "model-12408",
+        "fixed acidity": "7.4",
+        "volatile acidity": "0.70",
+        "citric acid": "0.00",
+        "residual sugar": "1.9",
+        "chlorides": "0.076",
+        "free sulfur dioxide": "11.0",
+        "total sulfur dioxide": "34.0",
+        "density": "0.9978",
+        "pH": "3.51",
+        "sulphates": "0.56",
+        "alcohol": "9.4"
+     }
+     ``
+     
+     * Returns: the predicted quality grade
+     
+     Once the model train this step can obviously be run with different input values without need to retain the model.
+ 
+ 
+ 
